8ca2a0eb0356d8f66fd75e5f25ae4bf8bd0b2682
Sarafirst <sarahmwadi15@gmail.com>
2022-11-24 13:32:31 -0800

    Free list

---
+++ 0x12-singly_linked_lists/2-add-node.c
@@ -0,0 +1,11 @@
+
+
+/**
+ * add_node - add new node begining of list_t
+ * @head: pointer to structure
+ * @str: string
+ * Return: the adrres of new element
+ */
+
+list_t *add_node(list_t **head, const char *str)
+
